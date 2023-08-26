ed8442acee9c7db857e61c83abb0ec8c7c7c5099
ArthurHetem <arthurhetemgames@gmail.com>
2023-08-25 22:06:53 -0300

    Implements flight import and export

--- app/Filament/Resources/FlightResource/Pages/ListFlights.php
+++ app/Filament/Resources/FlightResource/Pages/ListFlights.php
@@ -3,14 +3,16 @@
 namespace App\Filament\Resources\FlightResource\Pages;
 
 use App\Filament\Resources\FlightResource;
-use App\Models\Enums\ImportExportType;
+use App\Repositories\FlightRepository;
+use App\Services\ExportService;
 use App\Services\ImportService;
 use Filament\Actions;
+use Filament\Forms\Components\Toggle;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Forms\Components\FileUpload;
-use App\Http\Controllers\Admin\Traits\Importable;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class ListFlights extends ListRecords
 {
@@ -19,24 +21,36 @@ class ListFlights extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make()->label('Export to CSV'),
-            Actions\Action::make('import')->label('Import from CSV')->form([
-                FileUpload::make('importFile')->maxSize(1024 * 1024 * 10)->acceptedFileTypes(['text/csv'])->directory('import'),
-            ])->action(function (array $data): void {
-                dd($data);
-                /*$path = Storage::putFileAs(
-                    'import',
-                    $data['file'],
-                    'import_'.ImportExportType::label(ImportExportType::FLIGHTS).'.csv'
-                );
+            Actions\Action::make('export')->label('Export to CSV')
+                ->action(function (): BinaryFileResponse {
+                    $exporter = app(ExportService::class);
+                    $flightRepo = app(FlightRepository::class);
 
-                $importSvc = app(ImportService::class);
+                    $where = [];
+                    $file_name = 'flights.csv';
+                    $flights = $flightRepo->where($where)->orderBy('airline_id')->orderBy('flight_number')->orderBy('route_code')->orderBy('route_leg')->get();
 
-                $path = storage_path('app/'.$path);
-                Log::info('Uploaded airport import file to '.$path);
+                    $path = $exporter->exportFlights($flights);
 
-                $importSvc->importFlights($path, true);*/
-            }),
+                    return response()->download($path, $file_name, ['content-type' => 'text/csv'])->deleteFileAfterSend(true);
+                })->after(function (): void {
+                    Notification::make()
+                        ->title('Flights Exported')
+                        ->success()
+                        ->send();
+                }),
+            Actions\Action::make('import')->label('Import from CSV')
+                ->form([
+                    FileUpload::make('importFile')->acceptedFileTypes(['text/csv'])->disk('local')->directory('import'),
+                    Toggle::make('delete')->label('Delete Previous Flights')->default(false),
+                ])->action(function (array $data): void {
+                    $importSvc = app(ImportService::class);
+
+                    $path = storage_path('app/'.$data['importFile']);
+                    Log::info('Uploaded airport import file to '.$path);
+
+                    $importSvc->importFlights($path, $data['delete']);
+                }),
             Actions\CreateAction::make()->label('Add Flight'),
         ];
     }
