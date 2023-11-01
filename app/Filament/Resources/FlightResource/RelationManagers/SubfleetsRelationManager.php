1918c97bbf6e32f8c384bcd24f2945468165db1c
Arthur Parienté <arthur.pariente78960@gmail.com>
2023-11-01 15:33:33 +0100

    Allow to attach multiple subfleets to flight at once

--- app/Filament/Resources/FlightResource/RelationManagers/SubfleetsRelationManager.php
+++ app/Filament/Resources/FlightResource/RelationManagers/SubfleetsRelationManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\FlightResource\RelationManagers;
 
+use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
@@ -32,7 +33,9 @@ class SubfleetsRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\AttachAction::make()->icon('heroicon-o-plus-circle'),
+                Tables\Actions\AttachAction::make()->icon('heroicon-o-plus-circle')->recordSelect(function (Select $select) {
+                    return $select->multiple();
+                }),
             ])
             ->actions([
                 Tables\Actions\DetachAction::make(),
