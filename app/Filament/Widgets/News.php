837fb0273bc2955b72bca4f4e87305f6ebc224af
Arthur Pariente <arthur.pariente78960@gmail.com>
2023-11-23 12:22:35 +0100

    Disable polling in News Widget

--- app/Filament/Widgets/News.php
+++ app/Filament/Widgets/News.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Auth;
 
 class News extends BaseWidget
 {
+    protected static ?string $pollingInterval = null;
     public function table(Table $table): Table
     {
         return $table
