ed22fd8d315dafecf5377604dc562449d4a1f0ac
Arthur Pariente <arthur.pariente78960@gmail.com>
2023-11-26 11:39:33 +0100

    Update brand name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -62,6 +62,7 @@ class AdminPanelProvider extends PanelProvider
             ->plugins([
                 FilamentShieldPlugin::make(),
             ])
+            ->brandName("phpVMS")
             ->spa();
     }
 }
