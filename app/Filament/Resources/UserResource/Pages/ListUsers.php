ff750dd4e73e34b406159a37b85804534e47167c
Arthur Pariente <arthur.pariente78960@gmail.com>
2024-02-01 18:16:32 +0100

    Add InviteResource

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Filament\Resources\InviteResource;
 use App\Filament\Resources\UserFieldResource;
 use App\Filament\Resources\UserResource;
 use App\Filament\Resources\UserResource\Widgets\UserStats;
@@ -18,8 +19,14 @@ class ListUsers extends ListRecords
             Action::make('userfields')
                 ->label('User Fields')
                 ->icon('heroicon-o-clipboard-document-list')
-                ->url(UserFieldResource::getUrl('index'))
+                ->url(UserFieldResource::getUrl())
                 ->visible(fn (): bool => auth()->user()?->can('view_any_user::field')),
+
+            Action::make('invites')
+                ->label('Invites')
+                ->icon('heroicon-o-envelope')
+                ->url(InviteResource::getUrl())
+                ->visible(fn (): bool => auth()->user()?->can('view_any_invite') && setting('general.invite_only_registrations', false)),
         ];
     }
 
