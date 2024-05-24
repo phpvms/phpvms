ca43ed4c39cb55c70942f852aa5fea543f19b927
Arthur Pariente <arthur.pariente78960@gmail.com>
2024-05-24 10:38:45 +0200

    Add permissions check in ModuleLinksPlugin

--- resources/views/filament/plugins/module-links.blade.php
+++ resources/views/filament/plugins/module-links.blade.php
@@ -1,38 +1,40 @@
-<ul class="me-4 hidden items-center gap-x-4 lg:flex">
-  <x-filament-panels::topbar.item
-    :active="$current_panel->getId() === 'admin'"
-    icon="heroicon-o-home"
-    :url="url(\Filament\Facades\Filament::getPanel('admin')->getPath())"
-  >
-    Main v7
-  </x-filament-panels::topbar.item>
-
-  @foreach($panels as $panel)
-    @php
-      if ($panel->getId() === 'admin') {
-        continue;
-      }
-
-      $panel_name = ucfirst(str_replace('::admin', '', $panel->getId()));
-      $active = str_contains(url()->current(), strtolower($panel_name));
-      $icon = 'heroicon-o-puzzle-piece';
-    @endphp
+@can('view_module')
+  <ul class="me-4 hidden items-center gap-x-4 lg:flex">
     <x-filament-panels::topbar.item
-      :active="$active"
-      :icon="$icon"
-      :url="url($panel->getPath())"
+      :active="$current_panel->getId() === 'admin'"
+      icon="heroicon-o-home"
+      :url="url(\Filament\Facades\Filament::getPanel('admin')->getPath())"
     >
-      {{ $panel_name }}
+      Main v7
     </x-filament-panels::topbar.item>
-  @endforeach
+
+    @foreach($panels as $panel)
+      @php
+        if ($panel->getId() === 'admin') {
+          continue;
+        }
+
+        $panel_name = ucfirst(str_replace('::admin', '', $panel->getId()));
+        $active = str_contains(url()->current(), strtolower($panel_name));
+        $icon = 'heroicon-o-puzzle-piece';
+      @endphp
+      <x-filament-panels::topbar.item
+        :active="$active"
+        :icon="$icon"
+        :url="url($panel->getPath())"
+      >
+        {{ $panel_name }}
+      </x-filament-panels::topbar.item>
+    @endforeach
 
 
-  @foreach($old_links as $link)
-    <x-filament-panels::topbar.item
-      :active="false"
-      :url="$link['url']"
-    >
-      {{ $link['title'] }}
-    </x-filament-panels::topbar.item>
-  @endforeach
-</ul>
+    @foreach($old_links as $link)
+      <x-filament-panels::topbar.item
+        :active="false"
+        :url="$link['url']"
+      >
+        {{ $link['title'] }}
+      </x-filament-panels::topbar.item>
+    @endforeach
+  </ul>
+@endcan
