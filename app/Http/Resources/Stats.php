3f3c0592572c5d2a1fa94928c9fa2b0947608792
Julian Ramirez <90717365+rcomunica@users.noreply.github.com>
2024-11-27 23:34:54 -0500

    Update Stats.php

--- app/Http/Resources/Stats.php
+++ app/Http/Resources/Stats.php
@@ -17,24 +17,24 @@ class Stats extends Resource
         $avgLanding = DB::table('pireps')
             ->selectRaw('avg(landing_rate) as uresult')
             ->where('user_id', $this->id)
-            ->where('source', PirepSource::ACARS)->where('landing_rate', '<',0)
+            ->where('source', PirepSource::ACARS)->where('landing_rate', '<', 0)
             ->where('state', PirepState::ACCEPTED)
             ->value('uresult');
 
         $avgFuel = DB::table('pireps')
-        ->selectRaw('avg(fuel_used) as uresult')
-        ->where('user_id', $this->id)
-        ->where('state', PirepState::ACCEPTED)
-        ->value('uresult');
+            ->selectRaw('avg(fuel_used) as uresult')
+            ->where('user_id', $this->id)
+            ->where('state', PirepState::ACCEPTED)
+            ->value('uresult');
 
         $avgScore = DB::table('pireps')
-        ->selectRaw('avg(score) as uresult')
-        ->where('user_id', $this->id)
-        ->where('state', PirepState::ACCEPTED)
-        ->value('uresult');
+            ->selectRaw('avg(score) as uresult')
+            ->where('user_id', $this->id)
+            ->where('state', PirepState::ACCEPTED)
+            ->value('uresult');
 
         return [
-            'balance'       => $this->balance ?? 0,
+            'balance'       => $this->journal->balance->money->getValue() ?? 0,
             'avgScore'      => number_format($avgScore) ?? 0,
             'avgLanding'    => number_format($avgLanding) ?? 0,
             'avgFuel'       => number_format($avgFuel / 2.20462262185) . ' kg' ?? '',
