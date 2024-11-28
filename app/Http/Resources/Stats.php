e0eda0af793d2b59310fa6743a64e91780f8b7da
Julian Ramirez <90717365+rcomunica@users.noreply.github.com>
2024-11-27 22:24:15 -0500

    Create Stats.php

---
+++ app/Http/Resources/Stats.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Support\Facades\DB;
+use App\Contracts\Resource;
+use App\Models\Enums\PirepSource;
+use App\Models\Enums\PirepState;
+
+/**
+ * @mixin \App\Models\Stats
+ */
+class Stats extends Resource
+{
+    public function toArray($request)
+    {
+        $avgLanding = DB::table('pireps')
+            ->selectRaw('avg(landing_rate) as uresult')
+            ->where('user_id', $this->id)
+            ->where('source', PirepSource::ACARS)->where('landing_rate', '<',0)
+            ->where('state', PirepState::ACCEPTED)
+            ->value('uresult');
+
+        $avgFuel = DB::table('pireps')
+        ->selectRaw('avg(fuel_used) as uresult')
+        ->where('user_id', $this->id)
+        ->where('state', PirepState::ACCEPTED)
+        ->value('uresult');
+
+        $avgScore = DB::table('pireps')
+        ->selectRaw('avg(score) as uresult')
+        ->where('user_id', $this->id)
+        ->where('state', PirepState::ACCEPTED)
+        ->value('uresult');
+
+        return [
+            'balance'       => $this->balance ?? 0,
+            'avgScore'      => number_format($avgScore) ?? 0,
+            'avgLanding'    => number_format($avgLanding) ?? 0,
+            'avgFuel'       => number_format($avgFuel / 2.20462262185) . ' kg' ?? '',
+        ];
+    }
+}
