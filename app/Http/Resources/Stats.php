662470051fcbf476b7e634388cba4ca979ba7871
Julian Ramirez <90717365+rcomunica@users.noreply.github.com>
2024-12-01 01:01:47 -0500

    Update Stats.php

--- app/Http/Resources/Stats.php
+++ app/Http/Resources/Stats.php
@@ -3,9 +3,6 @@
 namespace App\Http\Resources;
 
 use App\Contracts\Resource;
-use App\Models\Enums\PirepSource;
-use App\Models\Enums\PirepState;
-use Illuminate\Support\Facades\DB;
 
 /**
  * @mixin \App\Models\Stats
@@ -14,30 +11,14 @@ class Stats extends Resource
 {
     public function toArray($request)
     {
-        $avgLanding = DB::table('pireps')
-            ->selectRaw('avg(landing_rate) as uresult')
-            ->where('user_id', $this->id)
-            ->where('source', PirepSource::ACARS)->where('landing_rate', '<', 0)
-            ->where('state', PirepState::ACCEPTED)
-            ->value('uresult');
-
-        $avgFuel = DB::table('pireps')
-            ->selectRaw('avg(fuel_used) as uresult')
-            ->where('user_id', $this->id)
-            ->where('state', PirepState::ACCEPTED)
-            ->value('uresult');
-
-        $avgScore = DB::table('pireps')
-            ->selectRaw('avg(score) as uresult')
-            ->where('user_id', $this->id)
-            ->where('state', PirepState::ACCEPTED)
-            ->value('uresult');
-
         return [
-            'balance'    => $this->journal->balance->money->getValue() ?? 0,
-            'avgScore'   => number_format($avgScore) ?? 0,
-            'avgLanding' => number_format($avgLanding) ?? 0,
-            'avgFuel'    => number_format($avgFuel / 2.20462262185).' kg' ?? '',
+            'flights'         => $this->flights,
+            'total_time'      => $this->flight_time,
+            'average_time'    => $this->average_flight_time,
+            'average_score'   => number_format($this->average_score),
+            'balance'         => $this->balance,
+            'average_fuel'    => number_format($this->average_fuel_used / 2.20462262185) . " Kg",
+            'average_landing' => number_format($this->average_landing_rate),
         ];
     }
 }
