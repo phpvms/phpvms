6a8caa9ee4b4b3b0ddf8a8c25088d2623d4307d6
Julian Ramirez <90717365+rcomunica@users.noreply.github.com>
2024-12-01 17:41:03 -0500

    FIX CI 2/2

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -192,7 +192,7 @@ class RouteServiceProvider extends ServiceProvider
     private function mapAdminRoutes()
     {
         Route::group([
-            'namespace'  => $this->namespace . '\\Admin',
+            'namespace'  => $this->namespace.'\\Admin',
             'prefix'     => 'admin',
             'as'         => 'admin.',
             'middleware' => ['web', 'auth', 'ability:admin,admin-access', EnableActivityLogging::class],
@@ -573,7 +573,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::group([
             'middleware' => ['api'],
-            'namespace'  => $this->namespace . '\\Api',
+            'namespace'  => $this->namespace.'\\Api',
             'prefix'     => 'api',
             'as'         => 'api.',
         ], function () {
