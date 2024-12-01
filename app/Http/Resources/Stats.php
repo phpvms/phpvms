5c2573843cd1549bb13a8ffc1f5789f23facd108
Julian Ramirez <90717365+rcomunica@users.noreply.github.com>
2024-12-01 01:08:17 -0500

    Update Stats.php

--- app/Http/Resources/Stats.php
+++ app/Http/Resources/Stats.php
@@ -17,7 +17,7 @@ class Stats extends Resource
             'average_time'    => $this->average_flight_time,
             'average_score'   => number_format($this->average_score),
             'balance'         => $this->balance,
-            'average_fuel'    => number_format($this->average_fuel_used / 2.20462262185)." Kg",
+            'average_fuel'    => number_format($this->average_fuel_used / 2.20462262185).' Kg',
             'average_landing' => number_format($this->average_landing_rate),
         ];
     }
