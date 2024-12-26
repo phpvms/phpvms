883e1c4fdc9e6464a813590e645ad24bb0dda980
ArthurHetem <arthurhetemgames@gmail.com>
2024-12-26 01:05:12 -0300

    refactor: migrate home to bs5

--- resources/views/layouts/seven/home.blade.php
+++ resources/views/layouts/seven/home.blade.php
@@ -4,40 +4,39 @@
 @section('content')
     <div class="row">
         <div class="col-sm-12">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-12 row">
-            <h2 class="display-5">@lang('common.newestpilots')</h2>
-            @foreach ($users as $user)
-                <div class="card" style="width: 18rem;">
-                    <div class="header header-primary text-center bg-primary">
-                        <h3 class="title title-up text-white">
-                            <a href="{{ route('frontend.profile.show', [$user->id]) }}"
-                                class="text-white">{{ $user->name_private }}</a>
-                        </h3>
-                        <div class="photo-container">
-                            @if ($user->avatar == null)
-                                <img class="rounded-circle" src="{{ $user->gravatar(123) }}">
-                            @else
-                                <img src="{{ $user->avatar->url }}" style="width: 123px;">
-                            @endif
-                        </div>
-                    </div>
-                    <div class="card-body bg-primary">
-                        <div class="social-description text-center text-white">
-                            <h2 class="description text-white">
-                                @if (filled($user->home_airport))
-                                    {{ $user->home_airport->icao }}
+            <div class="row">
+                <h2 class="display-5">@lang('common.newestpilots')</h2>
+            </div>
+            <div class="row">
+                @foreach ($users as $user)
+                    <div class="card col-2 bg-primary mx-1">
+                        <div class="header header-primary text-center pt-4">
+                            <h3 class="mb-4">
+                                <a href="{{ route('frontend.profile.show', [$user->id]) }}"
+                                    class="text-white text-decoration-none fw-bold text-uppercase">{{ $user->name_private }}</a>
+                            </h3>
+                            <div class="photo-container">
+                                @if ($user->avatar == null)
+                                    <img class="rounded-circle" src="{{ $user->gravatar(123) }}">
+                                @else
+                                    <img src="{{ $user->avatar->url }}" style="width: 123px;">
                                 @endif
-                            </h2>
+                            </div>
                         </div>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                        <a href="{{ route('frontend.profile.show', [$user->id]) }}"
-                            class="btn btn-neutral btn-sm">@lang('common.profile')</a>
+                        <div class="card-body mx-auto">
+                            <div class="text-center">
+                                <h2 class="fw-light text-white mb-4">
+                                    @if (filled($user->home_airport))
+                                        {{ $user->home_airport->icao }}
+                                    @endif
+                                </h2>
+                            </div>
+                            <a href="{{ route('frontend.profile.show', [$user->id]) }}"
+                                class="btn btn-secondary btn-sm">@lang('common.profile')</a>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
