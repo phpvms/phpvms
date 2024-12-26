d32a2e5daf73349d78c0954843d37d19970c500e
Taylor Broad <taylorbroad@gmail.com>
2024-12-26 10:51:56 +0900

    refactor: change database seed and add migration for default theme check

---
+++ app/Database/migrations/2024_12_26_014452_default_theme_setting_change.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Check in the DB if the general_theme setting exists. If so, check if the value is "default". If so, change it to "beta".
+        if (Schema::hasTable('settings')) {
+            $theme = DB::table('settings')->where('key', 'general_theme')->first();
+            if ($theme && $theme->value === 'default') {
+                DB::table('settings')->where('key', 'general_theme')->update(['value' => 'beta']);
+            }
+        }
+
+        // Check if the default theme exists physically. If it does, delete it.
+        if (file_exists(resource_path('views/layouts/default'))) {
+            File::deleteDirectory(resource_path('views/layouts/default'));
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
