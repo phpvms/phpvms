7433b025c7dfac659632588fdcf3a63670cf54a0
Arthur Pariente <arthur.pariente78960@gmail.com>
2024-12-27 10:02:31 +0100

    refactor: migrate flash to bs5

--- resources/views/layouts/seven/flash/message.blade.php
+++ resources/views/layouts/seven/flash/message.blade.php
@@ -9,17 +9,22 @@
           'body'       => $message['message']
       ])
     @else
-      <div class="alert alert-{{ $message['level'] }}
-           {{ $message['important'] ? 'alert-important' : '' }}"
-           role="alert">
-        @if ($message['important'])
-          <button type="button"
-                  class="close"
-                  data-bs-dismiss="alert"
-                  aria-hidden="true">&times;</button>
-        @endif
+      @php
+        $icon = match ($message['level']) {
+          'danger', 'warning' => 'exclamation-triangle-fill',
+          'success' => 'check-circle-fill',
+          default => 'info-circle-fill'
+        }
+      @endphp
+
+      <div class="alert alert-{{ $message['level'] }} d-flex align-items-center {{ $message['important'] ? 'alert-dismissible' : '' }}" role="alert">
+        <i class="bi bi-{{ $icon }} flex-shrink-0 me-2" role="img" aria-label="Info: "></i>
 
         {!! $message['message'] !!}
+
+        @if ($message['important'])
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        @endif
       </div>
     @endif
   @endif
