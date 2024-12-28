2370ed4d056af13a95c5bd4d7bcd15b6c8f19ffc
ArthurHetem <arthurhetemgames@gmail.com>
2024-12-28 14:14:52 -0300

    refactor: migrate user list to bs5

--- resources/views/layouts/seven/users/index.blade.php
+++ resources/views/layouts/seven/users/index.blade.php
@@ -2,15 +2,19 @@
 @section('title', trans_choice('common.pilot', 2))
 
 @section('content')
-  <div class="row">
-    <div class="col-md-12">
-      <h2>{{ trans_choice('common.pilot', 2) }}</h2>
-      @include('users.table')
+    <div class="row">
+        <div class="col-md-12">
+            <h2>{{ trans_choice('common.pilot', 2) }}</h2>
+            <div class="card">
+                <div class="card-body p-4">
+                    @include('users.table')
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-12 text-center">
-      {{ $users->withQueryString()->links('pagination.bootstrap-5') }}
+    <div class="row">
+        <div class="col-12 text-center">
+            {{ $users->withQueryString()->links('pagination.bootstrap-5') }}
+        </div>
     </div>
-  </div>
 @endsection
