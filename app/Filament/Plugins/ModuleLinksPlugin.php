87237edc675dd875ca2a7f9fe080749d83372367
Arthur Parienté <arthur.pariente78960@gmail.com>
2025-02-18 10:10:42 +0100

    Fix module navigation

--- app/Filament/Plugins/ModuleLinksPlugin.php
+++ app/Filament/Plugins/ModuleLinksPlugin.php
@@ -5,6 +5,8 @@ namespace App\Filament\Plugins;
 use App\Services\ModuleService;
 use Filament\Contracts\Plugin;
 use Filament\Facades\Filament;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 
 class ModuleLinksPlugin implements Plugin
@@ -21,11 +23,20 @@ class ModuleLinksPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
+        // Render in the topbar (wide screen)
         $panel->renderHook('panels::topbar.start', function () {
-            return view('filament.plugins.module-links', [
+            $group = $this->getGroup();
+
+            return view('filament.plugins.module-links-topbar', [
                 'current_panel' => Filament::getCurrentPanel(),
-                'panels'        => Filament::getPanels(),
-                'old_links'     => array_filter(app(ModuleService::class)->getAdminLinks(), static fn (array $link): bool => !str_contains($link['title'], 'Sample')),
+                'group'         => $this->getGroup(),
+            ]);
+        });
+
+        // Render in the sidebar (mobile)
+        $panel->renderHook('panels::sidebar.nav.end', function () {
+            return view('filament.plugins.module-links-sidebar', [
+                'group' => $this->getGroup(),
             ]);
         });
     }
