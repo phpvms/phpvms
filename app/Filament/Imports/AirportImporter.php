1c982d69d60d58ced36a1488214e51b1ff124119
Arthur Pariente <arthur.pariente78960@gmail.com>
2026-01-08 16:22:45 +0100

    add AirportImporter and AirportExporter

---
+++ app/Filament/Imports/AirportImporter.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Imports;
+
+use App\Models\Airport;
+use Filament\Actions\Imports\ImportColumn;
+use Filament\Actions\Imports\Importer;
+use Filament\Actions\Imports\Models\Import;
+use Illuminate\Support\Number;
+use Illuminate\Validation\Rule;
+use League\ISO3166\ISO3166;
+
+class AirportImporter extends Importer
+{
+    protected static ?string $model = Airport::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ImportColumn::make('iata'),
+            ImportColumn::make('icao')
+                ->fillRecordUsing(function (Airport $record, string $state): void {
+                    $record->id = $state;
+                    $record->icao = $state;
+                })
+                ->requiredMapping()
+                ->rules(['required', 'max:4']),
+            ImportColumn::make('name')
+                ->requiredMapping()
+                ->rules(['required']),
+            ImportColumn::make('location'),
+            ImportColumn::make('region'),
+            ImportColumn::make('country')
+                ->fillRecordUsing(function (Airport $record, string $state): void {
+                    $record->country = strtolower($state);
+                })
+                ->rules([Rule::in(array_column((new ISO3166())->all(), 'alpha2'))]),
+            ImportColumn::make('timezone'),
+            ImportColumn::make('hub')
+                ->requiredMapping()
+                ->boolean()
+                ->rules(['required', 'boolean']),
+            ImportColumn::make('notes'),
+            ImportColumn::make('lat')
+                ->numeric(decimalPlaces: 5)
+                ->rules(['numeric']),
+            ImportColumn::make('lon')
+                ->numeric(decimalPlaces: 5)
+                ->rules(['numeric']),
+            ImportColumn::make('elevation')
+                ->integer()
+                ->rules(['integer']),
+            ImportColumn::make('ground_handling_cost')
+                ->numeric()
+                ->rules(['numeric']),
+            ImportColumn::make('fuel_100ll_cost')
+                ->numeric()
+                ->rules(['numeric']),
+            ImportColumn::make('fuel_jeta_cost')
+                ->numeric()
+                ->rules(['numeric']),
+            ImportColumn::make('fuel_mogas_cost')
+                ->numeric()
+                ->rules(['numeric']),
+        ];
+    }
+
+    public function resolveRecord(): Airport
+    {
+        $record = Airport::withTrashed()->firstOrNew([
+            'id' => $this->data['icao'],
+        ]);
+
+        if ($record->trashed()) {
+            $record->restore();
+        }
+
+        return $record;
+    }
+
+    public static function getCompletedNotificationBody(Import $import): string
+    {
+        $body = 'Your airport import has completed and '.Number::format($import->successful_rows).' '.str('row')->plural($import->successful_rows).' imported.';
+
+        if (($failedRowsCount = $import->getFailedRowsCount()) !== 0) {
+            $body .= ' '.Number::format($failedRowsCount).' '.str('row')->plural($failedRowsCount).' failed to import.';
+        }
+
+        return $body;
+    }
+}
--- app/Filament/Resources/Airports/Pages/ListAirports.php
+++ app/Filament/Resources/Airports/Pages/ListAirports.php
@@ -2,11 +2,15 @@
 
 namespace App\Filament\Resources\Airports\Pages;
 
-use App\Filament\Actions\ExportAction;
-use App\Filament\Actions\ImportAction;
+use App\Filament\Actions\ExportAction as OldExportAction;
+use App\Filament\Actions\ImportAction as OldImportAction;
+use App\Filament\Exports\AirportExporter;
+use App\Filament\Imports\AirportImporter;
 use App\Filament\Resources\Airports\AirportResource;
 use App\Models\Enums\ImportExportType;
 use Filament\Actions\CreateAction;
+use Filament\Actions\ExportAction;
+use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Icons\Heroicon;
 
@@ -17,11 +21,20 @@ class ListAirports extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            ExportAction::make('export')
+            OldExportAction::make('export')
                 ->arguments(['resourceTitle' => 'airports', 'exportType' => ImportExportType::AIRPORT]),
 
-            ImportAction::make('import')
+            OldImportAction::make('import')
                 ->arguments(['resourceTitle' => 'airports', 'importType' => ImportExportType::AIRPORT]),
+
+            ImportAction::make('import')
+                ->visible(config('phpvms.use_queued_filament_imports'))
+                ->importer(AirportImporter::class),
+
+            ExportAction::make('export')
+                ->visible(config('phpvms.use_queued_filament_imports'))
+                ->exporter(AirportExporter::class),
+
             CreateAction::make()
                 ->icon(Heroicon::OutlinedPlusCircle),
         ];
