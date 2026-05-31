ee09d78beb1f8162a412c585f62562b2c5f7eefe
Nabeel Shahzad <99736+nabeelio@users.noreply.github.com>
2026-05-31 14:12:29 -0500

    Make the base seeder a little more robust

--- database/seeders/BaseDataSeeder.php
+++ database/seeders/BaseDataSeeder.php
@@ -6,13 +6,43 @@ namespace Database\Seeders;
 
 use App\Models\Kvp;
 use App\Models\Rank;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class BaseDataSeeder extends Seeder
 {
     public function run(): void
     {
-        if (Kvp::where('key', 'BaseDataSeeder')->exists()) {
+        $this->createInitialRanks();
+    }
+
+    /**
+     * Check if a specific key has been seeded
+     */
+    private function isSeeded(string $key): bool
+    {
+        $key .= '_seeded';
+
+        return Kvp::where('key', $key)->exists();
+    }
+
+    /**
+     * Mark a specific key as seeded
+     */
+    private function setSeeded(string $key): void
+    {
+        $key .= '_seeded';
+        Kvp::updateOrCreate(['key' => $key], ['value' => Carbon::now('UTC')->toDateTimeString()]);
+    }
+
+    /**
+     * Create initial ranks if they don't exist
+     */
+    private function createInitialRanks(): void
+    {
+        // Seems like they added ranks, so we don't need to do anything
+        // This is mainly a check for updates
+        if ($this->isSeeded('ranks') || Rank::count() > 1) {
             return;
         }
 
@@ -26,9 +56,6 @@ class BaseDataSeeder extends Seeder
             ],
         );
 
-        Kvp::create([
-            'key'   => 'BaseDataSeeder',
-            'value' => now()->toDateTimeString(),
-        ]);
+        $this->setSeeded('ranks');
     }
 }
