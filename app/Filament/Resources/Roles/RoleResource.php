4b4441cabb173859fa11bc0705b8b4b69bbdac0a
Nabeel Shahzad <99736+nabeelio@users.noreply.github.com>
2026-07-24 23:29:21 -0500

    Shift around navigation

--- app/Filament/Resources/Roles/RoleResource.php
+++ app/Filament/Resources/Roles/RoleResource.php
@@ -29,7 +29,7 @@ class RoleResource extends Resource
 
     protected static string|UnitEnum|null $navigationGroup = EnumsNavigationGroup::Config;
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 4;
 
     protected static ?string $slug = 'roles';
 
